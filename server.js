c0c8571358c4ffc0046ff55c8e8670bc3b4cf61e
Jassi797 <jasvinder797@gmail.com>
2018-03-29 19:30:51 +0530

    update1.1

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
+var transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  auth: {
+    user: 'ss4u.team.node@gmail.com',
+    pass: 'Marchend'
+  }
+}));
+ 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,"public")));
+app.post("/email",function(req,res){
+
+	var mailOptions = {
+	  from: 'divya@gmail.com',
+	  to: req.body.to,
+	  subject: req.body.subject,
+	  text: req.body.message
+	};
+
+	transporter.sendMail(mailOptions, function(error, info){
+	  if (error) {
+	    console.log(error);
+	  } else {
+	    console.log('Email sent: ' + info.response);
+	  }
+	}); 
+	console.log(req.body);
+	res.statuscode = 200;
+	res.write("hello");
+	res.end();
+})
+app.listen(8000);
